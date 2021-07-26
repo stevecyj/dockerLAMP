0bb19bc11fb3eb742e26296ca0e5134b33a0a1ed
root <root@nms6.vir999.com>
2021-07-26 11:14:26 +0800

    Fea:connect to mysql

--- php/src/index.php
+++ php/src/index.php
@@ -1,10 +1,4 @@
 <?php
-// echo '<h1>Hello Moto</h1>';
-// phpinfo();
-
-/**
- * db
- */
 //These are the defined authentication environment in the db service
 
 // The MySQL service named in the docker-compose.yml.
@@ -16,10 +10,23 @@ $user = 'MYSQL_USER';
 //database user password
 $pass = 'MYSQL_PASSWORD';
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
+// database name
+$mydatabase = 'MY_DATABASE';
+// check the mysql connection status
+
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
 }
